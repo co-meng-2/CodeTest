dbc2ad792fc44807875e921afbc9ebb6e93d7650
가보자가보자 <wo248655@gmail.com>
2024-03-06 21:34:50 +0900

    환승 실패(시간 너무 오래걸림)

---
+++ CodeTest/BOJ_환승.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#define fastio std::ios_base::sync_with_stdio(false);\
+cin.tie(NULL)
+using std::cin;
+using std::cout;
+
+#include <vector>
+using std::vector;
+
+#include <algorithm>
+#include <queue>
+
+using pii = std::pair<int, int>;
+// hyper 묶음으로 생각
+// 같은 hyper에 있다 -> 거리 + 1로 넣어줌.
+// [hyper][v] [v][hyper] 2개를 만들어줌.
+// 
+
+int main()
+{
+	fastio;
+
+	int v, cnt, hyper;
+	cin >> v >> cnt >> hyper;
+
+	vector<vector<int>> vh(v + 1);
+	vector<vector<int>> hv(v + 1);
+	for (int i = 1; i <= hyper; ++i)
+	{
+		for (int j = 0; j < cnt; ++j)
+		{
+			int input;
+			cin >> input;
+			vh[input].push_back(i);
+			hv[i].push_back(input);
+		}
+	}
+
+	vector<bool> visited(hyper + 1, false);
+	std::queue<pii> Q;
+	Q.push({ 1, 1});
+	int ans = -1;
+	while(!Q.empty())
+	{
+		auto [cur, w] = Q.front();
+		Q.pop();
+		if (cur == v)
+		{
+			ans = w;
+			break;
+		}
+			
+		for(auto ch : vh[cur])
+		{
+			if (visited[ch]) continue;
+			visited[ch] = true;
+			for(auto nv : hv[ch])
+			{
+				Q.push({ nv, w + 1 });
+			}
+		}
+	}
+
+	cout << ans;
+
+	return 0;
+}
+
+
+// S :
+// U :
+// T :
+// C :
+// Total :
