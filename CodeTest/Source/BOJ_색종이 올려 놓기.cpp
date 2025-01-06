abd4941b7ef39c2d254a9ee2d6fe308c9605ceb8
가보자가보자 <wo248655@gmail.com>
2025-01-06 10:45:01 +0900

    BOJ G4 색종이 올려 놓기
    위상정렬

---
+++ CodeTest/Source/BOJ_색종이 올려 놓기.cpp
@@ -0,0 +1,89 @@
+﻿#include <bits/stdc++.h>
+#define fastio std::ios_base::sync_with_stdio(false);\
+cin.tie(NULL)
+using namespace std;
+
+using ll = long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+
+int dx[4] = { -1,1,0,0 };
+int dy[4] = { 0,0,-1,1 };
+
+const int INF = 0x3f3f3f3f;
+
+int n;
+pii arr[100];
+int cnt[100];
+vector<int> adjList[100];
+
+int main()
+{
+	fastio;
+
+	cin >> n;
+	for(int i = 0; i < n; ++i)
+	{
+		cin >> arr[i].first >> arr[i].second;
+	}
+
+	for(int i = 0; i <n; ++i)
+	{
+		for(int j = i + 1; j < n; ++j)
+		{
+			auto [y1, x1] = arr[i];
+			auto [y2, x2] = arr[j];
+
+			if( (y1 <= y2 && x1 <= x2) || (y1 <= x2 && x1 <= y2 ))
+			{
+				adjList[i].push_back(j);
+				cnt[j]++;
+			}
+			else if((y1 >= y2 && x1 >= x2) || (y1 >= x2 && x1 >= y2 ))
+			{
+				adjList[j].push_back(i);
+				cnt[i]++;
+			}
+		}
+	}
+
+	queue<int> q;
+	for(int i = 0; i <n; ++i)
+	{
+		if(cnt[i] == 0)
+		{
+			q.push(i);
+		}
+	}
+
+	int depth = 0;
+	while(!q.empty())
+	{
+		int qsz = q.size();
+		while(qsz--)
+		{
+			int cur = q.front();
+			q.pop();
+
+			for(int nxt : adjList[cur])
+			{
+				if(--cnt[nxt] == 0)
+				{
+					q.push(nxt);
+				}
+			}
+		}
+		++depth;
+	}
+
+	cout << depth;
+
+	return 0;
+}
+
+
+// S : 
+// U : 
+// T : 
+// C :
+// Total :
