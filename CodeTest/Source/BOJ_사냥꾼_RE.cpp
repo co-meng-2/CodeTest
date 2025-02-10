771b3a98860c7da5170f8b3a8c36548c7b22e0aa
LEE JAE MYEANG <107743616+co-meng-2@users.noreply.github.com>
2025-02-10 10:04:16 +0900

    [Re] BOJ G4 사냥꾼 - Failed 대부분의 이분 탐색 유형인 T와 F의 구분점에 대해 묻는 문제가 아닌, FF/TTTT/FF와 같은 상황에서 T가 가능한지에 대해 묻고 있음.

---
+++ CodeTest/Source/BOJ_사냥꾼_RE.cpp
@@ -0,0 +1,78 @@
+﻿#include <bits/stdc++.h>
+#define fastio std::ios_base::sync_with_stdio(false);\
+cin.tie(NULL)
+using namespace std;
+
+using ll = long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+
+int dx[4] = { -1,1,0,0 };
+int dy[4] = { 0,0,-1,1 };
+
+const int INF = 0x3f3f3f3f;
+
+
+int M, N, L;
+int Points[100000];
+pii Animals[100000];
+
+// 거리 = L >= |a - x| + b
+
+int main()
+{
+	fastio;
+
+	cin >> M >> N >> L;
+
+	for(int i = 0; i < M; ++i)
+	{
+		cin >> Points[i];
+	}
+
+	for(int i = 0; i < N;  ++i)
+	{
+		cin >> Animals[i].first >> Animals[i].second;
+	}
+
+	sort(Points, Points + M);
+
+	int Ans = 0;
+	for(int i = 0; i < N; ++i)
+	{
+		int lo = 0;
+		int hi = M - 1;
+		
+		int l = Animals[i].first + Animals[i].second - L;
+		int r = Animals[i].first - Animals[i].second + L;
+
+		while(lo <= hi)
+		{
+			int mid = (lo + hi) / 2;
+			if(l <= Points[mid] && Points[mid] <= r)
+			{
+				++Ans;
+				break;
+			}
+			else if(Points[mid] < l)
+			{
+				lo = mid + 1;
+			}
+			else
+			{
+				hi = mid - 1;
+			}
+		}
+	}
+
+	cout << Ans;
+
+	return 0;
+}
+
+
+// S : 9:03
+// U : 9:05
+// T : Failed
+// C :
+// Total :
