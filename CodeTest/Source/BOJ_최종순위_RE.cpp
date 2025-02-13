f0ad39804ccbb3b632675b7aa37bd25324584a8e
LEE JAE MYEANG <107743616+co-meng-2@users.noreply.github.com>
2025-02-14 06:36:43 +0900

    [Re] BOJ G1 최종순위 위상정렬? 문제가 좀 쓰레기 인듯. 예시가 불친절 + 애매모호한 말장난. 확실한 순위를 찾을 수 없다 와 데이터에 일관성이 없다는 해석하기에 따라 다른 답이 도출될 수 있음. 제출자의 해석은 확실한 순위를 찾을 수 없다 자체가 데이터에 일관성이 없다에 먹혀 있다고 판단하여, 확실한 순위를 찾을 수 없는 경우를 고려하지 않아야 함.

---
+++ CodeTest/Source/BOJ_최종순위_RE.cpp
@@ -0,0 +1,92 @@
+﻿#include <bits/stdc++.h>
+#define fastio std::ios_base::sync_with_stdio(false);\
+cin.tie(NULL)
+using namespace std;
+
+using ll = long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+
+int dx[4] = { -1,1,0,0 };
+int dy[4] = { 0,0,-1,1 };
+
+const int INF = 0x3f3f3f3f;
+
+int T;
+
+int main()
+{
+	fastio;
+
+	cin >> T;
+	while(T--)
+	{
+		int N;
+		cin >> N;
+		vector<int> Teams(N);
+		vector<int> Degree(N + 1);
+		vector<int> OriginDegree;
+		
+		for(int i = 0; i < N; ++i)
+		{
+			cin >> Teams[i];
+			Degree[Teams[i]] = N - 1 - i; 
+		}
+		OriginDegree = Degree;
+
+		int ChangedCnt;
+		cin >> ChangedCnt;
+		vector<pii> Changed(ChangedCnt);
+		
+		for(int i = 0; i < ChangedCnt; ++i)
+		{
+			cin >> Changed[i].first >> Changed[i].second;
+		}
+		
+		for(auto [A, B] : Changed)
+		{
+			if(OriginDegree[A] > OriginDegree[B])
+			{
+				swap(A,B);
+			}
+			++Degree[A];
+			--Degree[B];
+		}
+
+		vector<int> Ans;
+		for(int i = N - 1; i >=0; --i)
+		{
+			for(int j = 1; j <= N; ++j)
+			{
+				if(Degree[j] == i)
+				{
+					Ans.push_back(j);
+					break;
+				}
+			}
+		}
+
+		if(Ans.size() != N)
+		{
+			cout << "IMPOSSIBLE\n";
+			continue;
+		}
+		
+		for(auto Elem : Ans)
+		{
+			cout << Elem << ' ';
+		}
+		cout << '\n';
+		
+	}
+	
+
+	return 0;
+}
+
+
+// S : 5 47 
+// U : 5 49 
+// T : 5 57
+// C : 6 31
+// Total : 44min
