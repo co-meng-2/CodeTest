b820923e096bf6a0d9f3cacd4df27829f350daa7
LEE JAE MYEANG <107743616+co-meng-2@users.noreply.github.com>
2025-02-27 09:48:44 +0900

    [Success] BOJ G2 가운데를 말해요 PQ 2개를 이용한 가운데 수를 맞추기.

---
+++ CodeTest/Source/BOJ_가운데를말해요.cpp
@@ -0,0 +1,74 @@
+﻿#include <bits/stdc++.h>
+#define fastio std::ios_base::sync_with_stdio(false);\
+cin.tie(NULL)
+using namespace std;
+
+using ll = long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+
+int dx[4] = { -1,1,0,0 };
+int dy[4] = { 0,0,-1,1 };
+
+const int INF = 0x3f3f3f3f;
+
+// 2개의 PQ를 준비 Inc Dec
+// 어떤 수가 들어왔을 때, 각 PQ의 Top을 비교하여, 어디에 넣어야 하는 수인지 판단.
+// Deq의 갯수가 Inc보다 많을 때, Inc로 Top을 빼서 넣어줌.
+// Inc의 Top을 출력
+
+int N;
+int Nums[100000];
+priority_queue<int> PQ_Inc;
+priority_queue<int, vector<int>, greater<>> PQ_Dec;
+
+int main()
+{
+	fastio;
+
+	cin >> N;
+	
+	for(int i = 0; i < N; ++i)
+	{
+		cin >> Nums[i];
+	}
+
+	PQ_Inc.push(Nums[0]);
+	cout << Nums[0] << '\n';
+
+	for(int i = 1; i < N; ++i)
+	{
+		int Num = Nums[i];
+		if(PQ_Inc.top() < Num)
+		{
+			PQ_Dec.push(Num);
+		}
+		else
+		{
+			PQ_Inc.push(Num);
+		}
+
+		if(PQ_Inc.size() < PQ_Dec.size())
+		{
+			PQ_Inc.push(PQ_Dec.top());
+			PQ_Dec.pop();
+		}
+
+		if(PQ_Dec.size() + 1 < PQ_Inc.size())
+		{
+			PQ_Dec.push(PQ_Inc.top());
+			PQ_Inc.pop();
+		}
+
+		cout << PQ_Inc.top() << '\n';
+	}
+	
+	return 0;
+}
+
+
+// S : 933
+// U : 934
+// T : 937
+// C : 947
+// Total : 14min
---
+++ CodeTest/Source/BOJ_바이러스공격.cpp
@@ -0,0 +1,128 @@
+﻿#include <bits/stdc++.h>
+#define fastio std::ios_base::sync_with_stdio(false);\
+cin.tie(NULL)
+using namespace std;
+
+using ll = long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+
+int dx[4] = {-1, 1, 0, 0};
+int dy[4] = {0, 0, -1, 1};
+
+const int INF = 0x3f3f3f3f;
+
+
+// 바이러스 전파 시간, 바이러스 지연 시간, 살포 지점 갯수, 건물 갯수
+
+int N, M;
+int A, B, C, D;
+char Board[1000][1000];
+
+const char Mark = '?';
+
+bool OOB(int y, int x)
+{
+    return y < 0 || x < 0 || y >= N || x >= M;
+}
+
+int main()
+{
+    fastio;
+
+    cin >> N >> M >> A >> B >> C >> D;
+
+    queue<pii> Virus;
+    queue<tiii> Delayed;
+
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j < M; ++j)
+        {
+            cin >> Board[i][j];
+
+            if (Board[i][j] == '*')
+            {
+                Virus.push({i, j});
+            }
+        }
+    }
+
+    int Time = 0;
+    while (Time < A)
+    {
+        int VirusCnt = Virus.size();
+        while (VirusCnt--)
+        {
+            auto [cy, cx] = Virus.front();
+            Virus.pop();
+
+            for (int i = 0; i < 4; ++i)
+            {
+                int ny = cy + dy[i];
+                int nx = cx + dx[i];
+
+                if (OOB(ny, nx) || Board[ny][nx] == Mark || Board[ny][nx] == '*')
+                {
+                    continue;
+                }
+
+                if (Board[ny][nx] == '#')
+                {
+                    Board[ny][nx] = Mark;
+                    Delayed.push({Time + B, ny, nx});
+                }
+                else
+                {
+                    Board[ny][nx] = '*';
+                    Virus.push({ny, nx});
+                }
+            }
+        }
+
+
+        while (!Delayed.empty())
+        {
+            auto [ct, cy, cx] = Delayed.front();
+            if (ct == Time)
+            {
+                Board[cy][cx] = '*';
+                Virus.push({cy, cx});
+                Delayed.pop();
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        ++Time;
+    }
+
+    bool IsEmpty = true;
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j < M; ++j)
+        {
+            if (Board[i][j] != '*')
+            {
+                cout << i + 1 << ' ' << j + 1 << '\n';
+                IsEmpty = false;
+            }
+        }
+    }
+
+    if(IsEmpty)
+    {
+        cout << -1;
+    }
+
+    return 0;
+}
+
+
+// S : 6 13
+// U : 6 16
+// T : 6 18
+// C : 6 38
+// Total : 25min
